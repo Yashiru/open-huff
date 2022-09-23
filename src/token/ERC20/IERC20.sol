70e7214d69a7f8fd247084471f58e824848ac7e5
Leo <leo.fasano@nested.finance>
2022-09-23 19:17:42 +0200

    feat: Approval event

--- src/token/ERC20/IERC20.sol
+++ src/token/ERC20/IERC20.sol
@@ -1,11 +1,12 @@
 interface IERC20 {
-	event Approve(address, address, uint256);
+	event Approval(address, address, uint256);
 	event Transfer(address, address, uint256);
 	function allowance(address, address) external view returns (uint256);
-	function approve(address, uint256) external;
+	function approve(address, uint256) external returns (bool);
 	function balanceOf(address) external view returns (uint256);
 	function decimals() external view returns (uint256);
-	function mint(address, uint256) external;
+	function decreaseAllowance(address, uint256) external;
+	function increaseAllowance(address, uint256) external;
 	function name() external view returns (string memory);
 	function owner() external view returns (address);
 	function symbol() external view returns (string memory);
